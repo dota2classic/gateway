7683a877b37023dd36bfee36e59d882b5146d8c8
itachi <enchantinggg4@gmail.com>
2020-10-04 23:26:04 +0300

    Leave queue command

---
+++ commands/player-leave-queue.command.ts
@@ -0,0 +1,6 @@
+import { PlayerId } from "../shared-types/player-id";
+import { MatchmakingMode } from "../shared-types/matchmaking-mode";
+
+export class PlayerLeaveQueueCommand {
+  constructor(public readonly playerID: PlayerId, public readonly mode: MatchmakingMode) {}
+}
