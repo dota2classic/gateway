3b91f16b34842e5b08cf924ec06102f0b760110a
itachi <enchantinggg4@gmail.com>
2020-10-04 21:41:33 +0300

    Ok lets try it

--- commands/player-enter-queue.command.ts
+++ commands/player-enter-queue.command.ts
@@ -1,5 +1,6 @@
 import { PlayerId } from "../shared-types/player-id";
+import { MatchmakingMode } from "../shared-types/matchmaking-mode";
 
 export class PlayerEnterQueueCommand {
-  constructor(public readonly playerID: PlayerId) {}
+  constructor(public readonly playerID: PlayerId, public readonly mode: MatchmakingMode) {}
 }
