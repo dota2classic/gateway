4b7d40cd15091d49ec273575592f7922c2c85d69
itachi <enchantinggg4@gmail.com>
2020-10-08 22:33:13 +0300

    Some stuff

---
+++ events/ready-check-started.event.ts
@@ -0,0 +1,8 @@
+import { ReadyCheckEntry } from "./room-ready-check-complete.event";
+
+export class ReadyCheckStartedEvent {
+  constructor(
+    public readonly roomId: string,
+    public readonly entries: ReadyCheckEntry[],
+  ) {}
+}
