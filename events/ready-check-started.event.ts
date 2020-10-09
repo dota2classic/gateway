db1953e9a24338302d56635536af466b16470789
itachi <enchantinggg4@gmail.com>
2020-10-09 04:10:48 +0300

    Yet again changed events

--- events/ready-check-started.event.ts
+++ events/ready-check-started.event.ts
@@ -1,10 +1,11 @@
-import {RoomReadyState} from "./room-ready-check-complete.event";
+import {ReadyCheckEntry, RoomReadyState} from "./room-ready-check-complete.event";
 import {MatchmakingMode} from "../shared-types/matchmaking-mode";
 
 export class ReadyCheckStartedEvent {
   constructor(
     public readonly roomId: string,
     public readonly mode: MatchmakingMode,
-    public readonly entries: RoomReadyState,
+    public readonly entries: ReadyCheckEntry[],
+    public readonly state: RoomReadyState,
   ) {}
 }
