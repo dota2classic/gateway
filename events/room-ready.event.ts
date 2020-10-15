7f98f94ead6bdcb65983138188f670151e0ee215
itachi <enchantinggg4@gmail.com>
2020-10-15 05:06:27 +0300

    Room ready event

--- events/room-ready.event.ts
+++ events/room-ready.event.ts
@@ -1,8 +1,12 @@
 import { MatchmakingMode } from "../shared-types/matchmaking-mode";
+import { PlayerId } from "../shared-types/player-id";
 
 export class RoomReadyEvent {
   constructor(
     public readonly roomId: string,
-    public readonly mode: MatchmakingMode
+    public readonly mode: MatchmakingMode,
+    public readonly radiant: PlayerId[],
+    public readonly dire: PlayerId[],
+    public readonly averageMMR: number,
   ) {}
 }
