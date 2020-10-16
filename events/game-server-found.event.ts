be073b9a51418dfe6a1424d041583d41ba9cd375
itachi <enchantinggg4@gmail.com>
2020-10-16 06:28:13 +0300

    Some stufff

--- events/game-server-found.event.ts
+++ events/game-server-found.event.ts
@@ -1,9 +1,11 @@
 import { Dota2Version } from '../shared-types/dota2version';
+import { MatchmakingMode } from '../shared-types/matchmaking-mode';
 
 export class GameServerFoundEvent {
   constructor(
     public readonly url: string,
     public readonly roomId: string,
     public readonly version: Dota2Version,
+    public readonly mode: MatchmakingMode
   ) {}
 }
