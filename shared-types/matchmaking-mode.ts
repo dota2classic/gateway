b5d47d45d9790007b6f9061de23fb3f4a986d154
itachi <enchantinggg4@gmail.com>
2020-10-20 20:10:53 +0300

    fix

--- shared-types/matchmaking-mode.ts
+++ shared-types/matchmaking-mode.ts
@@ -14,3 +14,14 @@ export const MatchmakingModes = [
   MatchmakingMode.RANKED,
   MatchmakingMode.UNRANKED,
 ]
+
+
+export const RoomSizes: { [key in MatchmakingMode]: number } = {
+  [MatchmakingMode.SOLOMID]: 2,
+  [MatchmakingMode.RANKED]: 10,
+  [MatchmakingMode.UNRANKED]: 10,
+  [MatchmakingMode.DIRETIDE]: 10,
+  [MatchmakingMode.GREEVILING]: 10,
+  [MatchmakingMode.TOURNAMENT]: 10,
+  [MatchmakingMode.ABILITY_DRAFT]: 10,
+};
