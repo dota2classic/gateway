81c159747144a1089e313cd159910544c7036443
itachi <enchantinggg4@gmail.com>
2020-10-20 20:06:27 +0300

    Match finished announce

--- events/match-finished.event.ts
+++ events/match-finished.event.ts
@@ -1,4 +1,6 @@
+import {MatchInfo} from "./room-ready.event";
+
 export class MatchFinishedEvent {
-  constructor(public readonly matchId: number) {
+  constructor(public readonly matchId: number, public readonly info: MatchInfo) {
   }
 }
