efc37bfbec1f42929b3d858e5b778b7ac83856a2
itachi <enchantinggg4@gmail.com>
2020-11-11 16:43:41 +0300

    Added avatars

--- queries/GetAll/get-all-query.result.ts
+++ queries/GetAll/get-all-query.result.ts
@@ -1,7 +1,7 @@
 import { PlayerId } from '../../shared-types/player-id';
 
 export class UserEntry {
-  constructor(public readonly id: PlayerId, public readonly name: string) {}
+  constructor(public readonly id: PlayerId, public readonly name: string, public readonly avatar: string) {}
 }
 export class GetAllQueryResult {
   constructor(public readonly entries: UserEntry[]) {}
--- queries/GetUserInfo/get-user-info-query.result.ts
+++ queries/GetUserInfo/get-user-info-query.result.ts
@@ -1,6 +1,9 @@
 import { PlayerId } from '../../shared-types/player-id';
 
 export class GetUserInfoQueryResult {
-  constructor(public readonly id: PlayerId, public readonly name: string) {
-  }
+  constructor(
+    public readonly id: PlayerId,
+    public readonly name: string,
+    public readonly avatar: string,
+  ) {}
 }
