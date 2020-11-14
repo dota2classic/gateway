d306828277c9820c86360c489687ef9bd6ffb148
itachi <enchantinggg4@gmail.com>
2020-11-15 00:49:53 +0300

    New UserInfo dto

--- queries/GetAll/get-all-query.result.ts
+++ queries/GetAll/get-all-query.result.ts
@@ -1,7 +1,13 @@
 import { PlayerId } from '../../shared-types/player-id';
+import { Role } from '../../shared-types/roles';
 
 export class UserEntry {
-  constructor(public readonly id: PlayerId, public readonly name: string, public readonly avatar: string) {}
+  constructor(
+    public readonly id: PlayerId,
+    public readonly name: string,
+    public readonly avatar: string,
+    public readonly roles: Role[]
+  ) {}
 }
 export class GetAllQueryResult {
   constructor(public readonly entries: UserEntry[]) {}
--- queries/GetUserInfo/get-user-info-query.result.ts
+++ queries/GetUserInfo/get-user-info-query.result.ts
@@ -1,9 +1,14 @@
 import { PlayerId } from '../../shared-types/player-id';
+import { UserEntry } from '../../queries/GetAll/get-all-query.result';
+import { Role } from '../../shared-types/roles';
 
-export class GetUserInfoQueryResult {
+export class GetUserInfoQueryResult extends UserEntry {
   constructor(
     public readonly id: PlayerId,
     public readonly name: string,
     public readonly avatar: string,
-  ) {}
+    public readonly roles: Role[]
+  ) {
+    super(id, name, avatar, roles)
+  }
 }
