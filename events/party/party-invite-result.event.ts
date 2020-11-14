f103fe28bce0b151feed1f814c388befee2e584b
itachi <enchantinggg4@gmail.com>
2020-11-14 19:54:39 +0300

    Fixed tests and added party invite result event

---
+++ events/party/party-invite-result.event.ts
@@ -0,0 +1,9 @@
+import {PlayerId} from "../../shared-types/player-id";
+
+export class PartyInviteResultEvent {
+  constructor(
+    public readonly inviteId: string,
+    public readonly invited: PlayerId,
+    public readonly accept: boolean,
+  ) {}
+}
