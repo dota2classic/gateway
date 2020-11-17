0e0fa33c360ab3fbca6dbf07b1017e77c29e0715
itachi <enchantinggg4@gmail.com>
2020-11-17 22:04:21 +0300

    GetConnections query

---
+++ queries/GetConnections/get-connections-query.result.ts
@@ -0,0 +1,6 @@
+import { ConnectionEntry } from '../GetAllConnections/get-all-connections-query.result';
+
+export class GetConnectionsQueryResult {
+  constructor(public readonly con?: ConnectionEntry) {
+  }
+}
---
+++ queries/GetConnections/get-connections.query.ts
@@ -0,0 +1,7 @@
+import { PlayerId } from 'shared-types/player-id';
+import { UserConnection } from 'shared-types/user-connection';
+
+export class GetConnectionsQuery {
+  constructor(public readonly playerId: PlayerId, public readonly connection: UserConnection) {
+  }
+}
