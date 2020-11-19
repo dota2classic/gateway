5737046123e0213750ae64609d32e9d894c80b94
itachi <enchantinggg4@gmail.com>
2020-11-19 04:49:07 +0300

    some stuff

--- queries/GetPlayerInfo/get-player-info-query.result.ts
+++ queries/GetPlayerInfo/get-player-info-query.result.ts
@@ -1,11 +1,19 @@
-import { PlayerId } from "../../shared-types/player-id";
-import { Dota2Version } from "../../shared-types/dota2version";
+import { PlayerId } from '../../shared-types/player-id';
+import { Dota2Version } from '../../shared-types/dota2version';
 
+export class PlayerOverviewSummary {
+  constructor(
+    public readonly rankedGamesPlayed: number,
+    public readonly totalWinrate: number,
+    public readonly rank: number
+  ) {}
+}
 export class GetPlayerInfoQueryResult {
   constructor(
     public readonly playerId: PlayerId,
     public readonly version: Dota2Version,
     public readonly mmr: number,
     public readonly recentWinrate: number,
+    public readonly summary: PlayerOverviewSummary
   ) {}
 }
