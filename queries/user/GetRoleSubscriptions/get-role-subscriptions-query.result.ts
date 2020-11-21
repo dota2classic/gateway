10fb9bd07a9abf5f775a84b326026aacb42ad17c
itachi <enchantinggg4@gmail.com>
2020-11-21 14:51:42 +0300

    Fixed

--- queries/user/GetRoleSubscriptions/get-role-subscriptions-query.result.ts
+++ queries/user/GetRoleSubscriptions/get-role-subscriptions-query.result.ts
@@ -1,5 +1,5 @@
-import { Role } from 'shared-types/roles';
-import { PlayerId } from 'shared-types/player-id';
+import { Role } from '../../../shared-types/roles';
+import { PlayerId } from '../../../shared-types/player-id';
 
 export class RoleSubscriptionEntry {
   end_time: number;
