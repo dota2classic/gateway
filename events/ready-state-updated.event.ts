eb1b092837ca77cd81f4c63895c448a1ab6d9fb6
itachi <enchantinggg4@gmail.com>
2020-12-08 00:04:10 +0300

    Added field to ready state updated event

--- events/ready-state-updated.event.ts
+++ events/ready-state-updated.event.ts
@@ -1,10 +1,11 @@
-import {RoomReadyState} from "./room-ready-check-complete.event";
+import {ReadyCheckEntry, RoomReadyState} from "./room-ready-check-complete.event";
 import {MatchmakingMode} from "../shared-types/matchmaking-mode";
 
 export class ReadyStateUpdatedEvent {
   constructor(
     public readonly roomID: string,
     public readonly mode: MatchmakingMode,
+    public readonly entries: ReadyCheckEntry[],
     public readonly state: RoomReadyState,
   ) {}
 }
