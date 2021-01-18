e5e7c9d457778de2e4662b8a01dda5c344065b26
itachi <enchantinggg4@gmail.com>
2021-01-18 23:08:34 +0300

    Ok bans for not loading kinda work now

--- shared-types/timings.ts
+++ shared-types/timings.ts
@@ -2,3 +2,8 @@ export const PARTY_INVITE_LIFETIME = 30_000
 export const ACCEPT_GAME_TIMEOUT = 120_000
 
 export const UNRANKED_GAMES_REQUIRED_FOR_RANKED = 3;
+
+
+export const NOT_LOAD_FIRST_BAN = 1000 * 60 * 10
+
+export const ADDITIVE_NOT_LOAD_BAN = 1000 * 60 * 60 * 4 // 4 hours
