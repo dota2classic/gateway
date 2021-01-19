024af05ff93292f25cd47b5e3a997622a1c1ee27
itachi <enchantinggg4@gmail.com>
2021-01-19 19:12:44 +0300

    fix fix

--- shared-types/timings.ts
+++ shared-types/timings.ts
@@ -1,7 +1,7 @@
 export const PARTY_INVITE_LIFETIME = 30_000;
 export const ACCEPT_GAME_TIMEOUT = 120_000;
 
-export const UNRANKED_GAMES_REQUIRED_FOR_RANKED = 3;
+export const UNRANKED_GAMES_REQUIRED_FOR_RANKED = 1;
 
 export const NOT_LOAD_FIRST_BAN = 1000 * 60 * 10;
 
