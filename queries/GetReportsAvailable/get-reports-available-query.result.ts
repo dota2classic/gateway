e18ef295a71b7b456a40a04ad5dfb34d74681295
itachi <enchantinggg4@gmail.com>
2021-02-02 05:35:36 +0300

    Fix

--- queries/GetReportsAvailable/get-reports-available-query.result.ts
+++ queries/GetReportsAvailable/get-reports-available-query.result.ts
@@ -1,4 +1,6 @@
+import { PlayerId } from '../../shared-types/player-id';
+
 export class GetReportsAvailableQueryResult {
-  constructor(public readonly available: number) {
+  constructor(public readonly id: PlayerId, public readonly available: number) {
   }
 }
