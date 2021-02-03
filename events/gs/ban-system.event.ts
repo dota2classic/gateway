5b7e1ba0e6a20c521d41ab2555a52fbe25d7a3b7
itachi <enchantinggg4@gmail.com>
2021-02-03 22:14:20 +0300

    Added debug event for things

---
+++ events/gs/ban-system.event.ts
@@ -0,0 +1,17 @@
+import { PlayerId } from '../../shared-types/player-id';
+import { BanReason } from '../../shared-types/ban';
+
+export class BanSystemEntry {
+  constructor(
+    public readonly reason: BanReason,
+    public readonly time: number,
+  ) {}
+}
+export class BanSystemEvent {
+  constructor(
+    public readonly id: PlayerId,
+    public readonly entries: BanSystemEntry[],
+    public readonly resultBanDate: number,
+    public readonly banDuration: number
+  ) {}
+}
