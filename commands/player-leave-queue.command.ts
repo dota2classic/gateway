bea51c85d9c4bd2414d2ab3310d2991ec752b1bb
itachi <enchantinggg4@gmail.com>
2021-05-09 19:49:11 +0300

    Yes

--- commands/player-leave-queue.command.ts
+++ commands/player-leave-queue.command.ts
@@ -1,7 +1,11 @@
-import { PlayerId } from "../shared-types/player-id";
-import { MatchmakingMode } from "../shared-types/matchmaking-mode";
+import {PlayerId} from "../shared-types/player-id";
+import {MatchmakingMode} from "../shared-types/matchmaking-mode";
+import {Dota2Version} from "../shared-types/dota2version";
 
 export class PlayerLeaveQueueCommand {
-  constructor(public readonly playerID: PlayerId, public readonly mode: MatchmakingMode) {}
+  constructor(
+    public readonly playerID: PlayerId,
+    public readonly mode: MatchmakingMode,
+    public readonly version: Dota2Version,
+  ) {}
 }
-
