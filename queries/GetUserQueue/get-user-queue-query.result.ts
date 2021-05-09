821f8194afcc9c85f13f04558f1e8d8850dceff7
itachi <enchantinggg4@gmail.com>
2021-05-09 18:36:38 +0300

    getuserqueue query

--- queries/GetUserQueue/get-user-queue-query.result.ts
+++ queries/GetUserQueue/get-user-queue-query.result.ts
@@ -1,5 +1,6 @@
 import {MatchmakingMode} from "../../shared-types/matchmaking-mode";
+import {Dota2Version} from "../../shared-types/dota2version";
 
 export class GetUserQueueQueryResult {
-  constructor(public readonly mode?: MatchmakingMode) {}
+  constructor(public readonly mode?: MatchmakingMode, public readonly version?: Dota2Version) {}
 }
