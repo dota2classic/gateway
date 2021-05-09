a61fa899b6720727c5a7de72e6998fc352530e89
itachi <enchantinggg4@gmail.com>
2021-05-09 21:11:14 +0300

    Tournaments

--- events/tournament/tournament-game-ready.event.ts
+++ events/tournament/tournament-game-ready.event.ts
@@ -1,12 +1,14 @@
 import { MatchmakingMode } from '../../shared-types/matchmaking-mode';
 import { PlayerId } from '../../shared-types/player-id';
 import { BracketEntryType } from '../../shared-types/tournament';
+import { Dota2Version } from '../../shared-types/dota2version';
 
 export class TournamentGameReadyEvent {
   constructor(
     public readonly tournamentId: number,
     public readonly tourMatchId: number,
     public readonly mode: MatchmakingMode,
+    public readonly version: Dota2Version,
     public readonly radiant: PlayerId[],
     public readonly dire: PlayerId[]
   ) {
