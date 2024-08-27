41dcfff7e9b010514cc03a8a0b517a99a3b25b47
Yuri Stepin <enchantinggg4@gmail.com>
2024-08-27 17:38:28 +0300

    Some logging

--- shared-types/player-id.ts
+++ shared-types/player-id.ts
@@ -1,7 +1,7 @@
 export class PlayerId {
   constructor(public readonly value: string) {
     if (Number.isNaN(parseInt(value))) {
-      throw new Error("Wrong id");
+      throw new Error(`Wrong id: expected numerical string, got '${value}'`);
     }
   }
 }
