b7ce38615ba913c62bd20c2c19fc9b42cb48ad41
Yuri Stepin <enchantinggg4@gmail.com>
2024-09-02 16:58:25 +0300

    Fix event

--- commands/LaunchGameServer/launch-game-server.command.ts
+++ commands/LaunchGameServer/launch-game-server.command.ts
@@ -1,14 +1,13 @@
 import { MatchmakingMode } from '../../shared-types/matchmaking-mode';
 import { Dota2Version } from '../../shared-types/dota2version';
-import { PlayerId } from '../../shared-types/player-id';
-import { DotaTeam } from '../../shared-types/dota-team';
+import { MatchPlayer } from '../../events/room-ready.event';
 
 export class LaunchGameServerCommand {
-  constructor(public readonly url: string,
-              public readonly matchId: number,
-              public readonly info: GSMatchInfo
-  ) {
-  }
+  constructor(
+    public readonly url: string,
+    public readonly matchId: number,
+    public readonly info: GSMatchInfo,
+  ) {}
 }
 
 export class GSMatchInfo {
@@ -17,19 +16,7 @@ export class GSMatchInfo {
     public readonly roomId: string,
     public readonly players: MatchPlayer[],
     public readonly version: Dota2Version,
-
-
-    public readonly radiant: PlayerId[],
-    public readonly dire: PlayerId[],
     public readonly averageMMR: number,
   ) {}
 }
 
-export class MatchPlayer{
-  constructor(
-    public readonly playerId: PlayerId,
-    public readonly name: string,
-    public readonly team: DotaTeam
-  ) {
-  }
-}
--- events/room-ready.event.ts
+++ events/room-ready.event.ts
@@ -1,13 +1,22 @@
 import { MatchmakingMode } from '../shared-types/matchmaking-mode';
 import { PlayerId } from '../shared-types/player-id';
 import { Dota2Version } from '../shared-types/dota2version';
+import { DotaTeam } from '../shared-types/dota-team';
+
+export class MatchPlayer {
+  constructor(
+    public readonly playerId: PlayerId,
+    public readonly name: string,
+    public readonly team: DotaTeam,
+  ) {}
+}
+
 
 export class MatchInfo {
   constructor(
     public readonly mode: MatchmakingMode,
     public readonly roomId: string,
-    public readonly radiant: PlayerId[],
-    public readonly dire: PlayerId[],
+    public readonly players: MatchPlayer[],
     public readonly averageMMR: number,
     public readonly version: Dota2Version,
     public readonly tournamentId?: number,
@@ -19,8 +28,7 @@ export class RoomReadyEvent {
   constructor(
     public readonly roomId: string,
     public readonly mode: MatchmakingMode,
-    public readonly radiant: PlayerId[],
-    public readonly dire: PlayerId[],
+    public readonly players: MatchPlayer[],
     public readonly averageMMR: number,
     public readonly version: Dota2Version
   ) {}
