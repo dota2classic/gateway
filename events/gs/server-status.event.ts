7e79c944560b6d4244721d3174ee04f85b22f262
Itachi <enchantinggg4@gmail.com>
2024-09-03 17:05:58 +0300

    Adde server status event

---
+++ events/gs/server-status.event.ts
@@ -0,0 +1,10 @@
+import { GSMatchInfo } from "../../commands/LaunchGameServer/launch-game-server.command";
+
+export class ServerStatusEvent {
+    constructor(
+        public readonly url: string,
+        public readonly running: boolean,
+        public readonly matchId: number | undefined,
+        public readonly session: GSMatchInfo | undefined
+    ){}
+}
