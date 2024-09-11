812304ddf5a1c3b8945eb968dc5c71a2c1bd439f
Itachi <enchantinggg4@gmail.com>
2024-09-11 21:52:49 +0300

    live match

--- events/gs/live-match-update.event.ts
+++ events/gs/live-match-update.event.ts
@@ -1,3 +1,4 @@
+import { Dota_GameMode } from '../../shared-types/dota-game-mode';
 import { MatchmakingMode } from '../../shared-types/matchmaking-mode';
 
 class PlayerInfo {
@@ -5,18 +6,36 @@ class PlayerInfo {
   team: number;
   steam_id: string;
   level: number;
-  pos_x: number;
+
   bot: boolean;
+
+  pos_x: number;
   pos_y: number;
-  items: string[];
+  angle: number
+
+  mana: number
+  max_mana: number
+  health: number
+  max_health: number
+
+  item0: number;
+  item1: number;
+  item2: number;
+  item3: number;
+  item4: number;
+  item5: number;
+
   kills: number;
   deaths: number;
   assists: number;
+
+  respawn_time: number;
 }
 
 export class LiveMatchUpdateEvent {
   matchId: number;
-  type: MatchmakingMode;
+  matchmaking_mode: MatchmakingMode
+  game_mode: Dota_GameMode
   duration: number;
   server: string;
   timestamp: number;
