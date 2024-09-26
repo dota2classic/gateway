bd75f58089e90d533483d72e78dc34d4730394f4
Yuri Stepin <enchantinggg4@gmail.com>
2024-09-26 12:48:38 +0300

    Event cleanup

--- commands/LaunchGameServer/launch-game-server.command.ts
+++ commands/LaunchGameServer/launch-game-server.command.ts
@@ -1,6 +1,7 @@
 import { MatchmakingMode } from '../../shared-types/matchmaking-mode';
 import { Dota2Version } from '../../shared-types/dota2version';
 import { MatchPlayer } from '../../events/room-ready.event';
+import { Dota_GameMode } from '../../shared-types/dota-game-mode';
 
 export class LaunchGameServerCommand {
   constructor(
@@ -13,6 +14,7 @@ export class LaunchGameServerCommand {
 export class GSMatchInfo {
   constructor(
     public readonly mode: MatchmakingMode,
+    public readonly gameMode: Dota_GameMode,
     public readonly roomId: string,
     public readonly players: MatchPlayer[],
     public readonly version: Dota2Version,
