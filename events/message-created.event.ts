fb678201de4ebc498aea98a98abdd2802dcc5b29
Yuri Stepin <enchantinggg4@gmail.com>
2024-09-30 22:13:45 +0300

    External thread id

--- events/message-created.event.ts
+++ events/message-created.event.ts
@@ -1,10 +1,11 @@
 export class MessageCreatedEvent {
-         constructor(
-           public readonly threadId: string,
-           public readonly messageId: string,
-           public readonly authorId: string,
-           public readonly createdAt: string,
-           public readonly content: string,
-           public readonly messageIndex: number,
-         ) {}
-       }
+  constructor(
+    public readonly threadId: string,
+    public readonly externalThreadId: string,
+    public readonly messageId: string,
+    public readonly authorId: string,
+    public readonly createdAt: string,
+    public readonly content: string,
+    public readonly messageIndex: number,
+  ) {}
+}
