a31286b4d79e809ff62d122cd2cdf4a5eabb29ce
Yuri Stepin <enchantinggg4@gmail.com>
2024-10-25 10:15:44 +0300

    fixes

--- events/message-updated.event.ts
+++ events/message-updated.event.ts
@@ -2,6 +2,8 @@ export class MessageUpdatedEvent {
   constructor(
     public readonly threadId: string,
     public readonly messageId: string,
+    public readonly authorId: string,
+    public readonly createdAt: string,
     public readonly content: string,
     public readonly messageIndex: number,
     public readonly deleted: boolean
