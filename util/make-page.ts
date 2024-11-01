900ac1877d41a0a708bd4fdec9d93a5910cc64a6
Yuri Stepin <enchantinggg4@gmail.com>
2024-11-01 07:49:35 +0300

    Test

---
+++ util/make-page.ts
@@ -0,0 +1,16 @@
+import { Page } from '../shared-types/page';
+
+export function makePage<R, T = R>(
+  items: R[],
+  total: number,
+  page: number,
+  perPage: number,
+  mapper: (R) => T = (x) => x,
+): Page<T> {
+  return {
+    data: items.map(mapper),
+    page,
+    perPage: perPage,
+    pages: Math.ceil(total / perPage),
+  };
+}
