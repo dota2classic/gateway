0cec9ecb811a65d8c7812ea552927f56cf6dbde9
Itachi <enchantinggg4@gmail.com>
2024-11-04 17:44:30 +0300

    good

---
+++ events/match-failed.event.ts
@@ -0,0 +1,7 @@
+import { PlayerId } from "../shared-types/player-id";
+
+export class MatchFailedEvent {
+    constructor(public readonly matchId: number, public readonly serverUrl: string, public readonly failedPlayers: PlayerId[]){
+
+    }
+}
