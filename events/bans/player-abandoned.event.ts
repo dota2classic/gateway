c001ef2c2d656947be5767f2aee1ec75185bf4af
Itachi <enchantinggg4@gmail.com>
2024-11-11 17:17:13 +0300

    fixes

--- events/bans/player-abandoned.event.ts
+++ events/bans/player-abandoned.event.ts
@@ -1,8 +1,10 @@
+import { MatchmakingMode } from '../../shared-types/matchmaking-mode';
 import { PlayerId } from '../../shared-types/player-id';
 
 export class PlayerAbandonedEvent {
   constructor(
     public readonly playerId: PlayerId,
     public readonly matchId: number,
+    public readonly mode: MatchmakingMode
   ) {}
 }
--- events/bans/player-not-loaded.event.ts
+++ events/bans/player-not-loaded.event.ts
@@ -1,5 +1,5 @@
 import { PlayerId } from '../../shared-types/player-id';
-import { MatchmakingMode } from 'shared-types/matchmaking-mode';
+import { MatchmakingMode } from '../../shared-types/matchmaking-mode';
 
 export class PlayerNotLoadedEvent {
   constructor(
