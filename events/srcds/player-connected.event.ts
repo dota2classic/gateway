57b3b71c7f10fe82e0eb9ce4ac04df04ad05f433
Itachi <enchantinggg4@gmail.com>
2024-12-06 01:16:25 +0300

    fdf

---
+++ events/srcds/player-connected.event.ts
@@ -0,0 +1,9 @@
+import { PlayerId } from "shared-types/player-id";
+
+export class PlayerConnectedEvent {
+    constructor(
+        public readonly playerId: PlayerId,
+        public readonly matchId: number,
+        public readonly serverUrl: string
+    ) {}
+}
