dcef972d668305e046a4b88607e829c3822758b8
Yuri Stepin <enchantinggg4@gmail.com>
2024-12-22 14:55:12 +0300

    Fixed tests

--- events/lobby-ready.event.ts
+++ events/lobby-ready.event.ts
@@ -2,11 +2,13 @@ import { MatchmakingMode } from "../shared-types/matchmaking-mode";
 import { Dota2Version } from "../shared-types/dota2version";
 import { MatchPlayer } from "./room-ready.event";
 import { Dota_GameMode } from "../shared-types/dota-game-mode";
+import { Dota_Map } from "../shared-types/dota-map";
 
 export class LobbyReadyEvent {
   constructor(
     public readonly roomId: string,
     public readonly mode: MatchmakingMode,
+    public readonly map: Dota_Map,
     public readonly gameMode: Dota_GameMode,
     public readonly players: MatchPlayer[],
     public readonly version: Dota2Version,
