35b81b99160856b02980b512c304c5a1b401c8e6
Yuri Stepin <enchantinggg4@gmail.com>
2024-12-29 02:03:29 +0300

    basic Reactions

--- events/message-updated.event.ts
+++ events/message-updated.event.ts
@@ -1,10 +1,13 @@
+import { ReactionEntry } from '../../forum/dto/message.dto';
+
 export class MessageUpdatedEvent {
   constructor(
     public readonly threadId: string,
-    public readonly messageId: string,
-    public readonly authorId: string,
+    public readonly id: string,
+    public readonly author: string,
     public readonly createdAt: string,
     public readonly content: string,
-    public readonly deleted: boolean
+    public readonly deleted: boolean,
+    public readonly reactions: ReactionEntry[],
   ) {}
 }
