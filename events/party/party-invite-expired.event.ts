d41dddd5aa25916d13c9dcc5d6450329b4be6769
Yuri Stepin <enchantinggg4@gmail.com>
2025-01-20 03:25:24 +0300

    Good progress

--- events/party/party-invite-expired.event.ts
+++ events/party/party-invite-expired.event.ts
@@ -1,10 +1,3 @@
-import { PlayerId } from "../../shared-types/player-id";
-
 export class PartyInviteExpiredEvent {
-  constructor(
-    public readonly id: string,
-    public readonly invited: PlayerId,
-    public readonly partyId: string,
-    public readonly inviter: PlayerId,
-  ) {}
+  constructor(public readonly id: string) {}
 }
