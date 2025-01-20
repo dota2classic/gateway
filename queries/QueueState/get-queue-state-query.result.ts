93568093b35d1edd789095c942139192cca8a19e
Yuri Stepin <enchantinggg4@gmail.com>
2025-01-20 16:16:13 +0300

    stuff

--- queries/QueueState/get-queue-state-query.result.ts
+++ queries/QueueState/get-queue-state-query.result.ts
@@ -1,14 +1,11 @@
 import { MatchmakingMode } from "../../shared-types/matchmaking-mode";
 import { PartyId } from "../../shared-types/party-id";
-import { PlayerId } from "../../shared-types/player-id";
 
 interface GatewayQueueStateQuery_QueueEntry {
   readonly partyID: PartyId;
-  readonly players: PlayerId[];
+  readonly players: string[];
+  readonly modes: MatchmakingMode[];
 }
 export class GetQueueStateQueryResult {
-  constructor(
-    public readonly mode: MatchmakingMode,
-    public readonly entries: GatewayQueueStateQuery_QueueEntry[],
-  ) {}
+  constructor(public readonly entries: GatewayQueueStateQuery_QueueEntry[]) {}
 }
