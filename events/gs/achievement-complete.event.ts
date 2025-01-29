1324f2ed66d8627d1dd749b1304e97296d115dd9
Yuri Stepin <enchantinggg4@gmail.com>
2025-01-29 11:40:25 +0300

    Achievment complete made public

---
+++ events/gs/achievement-complete.event.ts
@@ -0,0 +1,10 @@
+import { AchievementKey } from '../../shared-types/achievemen-key';
+
+export class AchievementCompleteEvent {
+  constructor(
+    public readonly achievement: AchievementKey,
+    public readonly playerId: string,
+    public readonly hero: string,
+    public readonly matchId: number,
+  ) {}
+}
