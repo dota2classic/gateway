1af186af36745413b30a02c70a904dbb1ec2253b
Yuri Stepin <enchantinggg4@gmail.com>
2025-02-09 03:23:55 +0300

    To be continued tomorrow

--- events/gs/server-status.event.ts
+++ events/gs/server-status.event.ts
@@ -1,10 +1,6 @@
-import { GSMatchInfo } from "../../commands/LaunchGameServer/launch-game-server.command";
-
 export class ServerStatusEvent {
   constructor(
     public readonly url: string,
     public readonly running: boolean,
-    public readonly matchId: number | undefined,
-    public readonly session: GSMatchInfo | undefined,
   ) {}
 }
