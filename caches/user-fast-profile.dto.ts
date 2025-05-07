f1d9a45ae4ae236cceeeb01284763f591e073884
Yuri Stepin <enchantinggg4@gmail.com>
2025-05-07 08:56:19 +0300

    Extend model

--- caches/user-fast-profile.dto.ts
+++ caches/user-fast-profile.dto.ts
@@ -1,8 +1,14 @@
 import { Role } from '../shared-types/roles';
+import { UserConnection } from 'shared-types/user-connection';
 
+export class ExternalUserConnection {
+  connection: UserConnection;
+  externalId: string;
+}
 export class UserFastProfileDto {
   steamId: string;
   name: string;
   avatar: string;
   roles: Role[];
+  connections: ExternalUserConnection[];
 }
--- shared-types/user-connection.ts
+++ shared-types/user-connection.ts
@@ -1,3 +1,4 @@
 export enum UserConnection {
   DISCORD = "DISCORD",
+  TWITCH = "TWITCH"
 }
