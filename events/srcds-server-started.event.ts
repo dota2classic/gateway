98020bb8f7005f9d53caec265438097c7c083807
Yuri Stepin <enchantinggg4@gmail.com>
2025-05-21 14:57:40 +0300

    Stuff

--- events/srcds-server-started.event.ts
+++ events/srcds-server-started.event.ts
@@ -1,9 +1,8 @@
-import { GSMatchInfo } from '../commands/LaunchGameServer/launch-game-server.command';
+import { LaunchGameServerCommand } from '../commands/LaunchGameServer/launch-game-server.command';
 
 export class SrcdsServerStartedEvent {
   constructor(
     public readonly server: string,
-    public readonly matchId: number,
-    public readonly info: GSMatchInfo,
+    public readonly info: LaunchGameServerCommand,
   ) {}
 }
