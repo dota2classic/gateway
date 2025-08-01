a489badf24a275312bef2af8c2ec51fb4698a1e2
Yuri Stepin <enchantinggg4@gmail.com>
2025-08-01 18:37:39 +0300

    Event

--- events/item-dropped.event.ts
+++ events/item-dropped.event.ts
@@ -2,7 +2,8 @@ export class ItemDroppedEvent {
   constructor(
     public readonly matchId: number,
     public readonly steamId: string,
-    public readonly assetId: string
+    public readonly assetId: string,
+    public readonly marketHashName: string
   ) {
   }
 }
