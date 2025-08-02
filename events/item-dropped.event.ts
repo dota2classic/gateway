f6b52e28e85b1a0d0b9ef1a331ce95e4c54d7b06
Yuri Stepin <enchantinggg4@gmail.com>
2025-08-02 03:12:54 +0300

    It works!

--- events/item-dropped.event.ts
+++ events/item-dropped.event.ts
@@ -1,9 +1,18 @@
+export class MarketItemDto {
+  constructor(
+    public readonly marketHashName: string,
+    public readonly quality: string,
+    public readonly price: number,
+    public readonly icon: string,
+    public readonly type: string
+  ) {}
+}
+
 export class ItemDroppedEvent {
   constructor(
     public readonly matchId: number,
     public readonly steamId: string,
     public readonly assetId: string,
-    public readonly marketHashName: string
-  ) {
-  }
+    public readonly item: MarketItemDto,
+  ) {}
 }
