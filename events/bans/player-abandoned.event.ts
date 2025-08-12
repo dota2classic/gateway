05bd4263dbd1f1d8f0f7e1c56a0f20553e28c172
Yuri Stepin <enchantinggg4@gmail.com>
2025-08-12 21:03:17 +0300

    Extend abandon event

--- events/bans/player-abandoned.event.ts
+++ events/bans/player-abandoned.event.ts
@@ -1,5 +1,6 @@
-import { MatchmakingMode } from "../../shared-types/matchmaking-mode";
-import { PlayerId } from "../../shared-types/player-id";
+import { MatchmakingMode } from '../../shared-types/matchmaking-mode';
+import { PlayerId } from '../../shared-types/player-id';
+import { Dota_GameRulesState } from '../../shared-types/dota-game-rules-state';
 
 export class PlayerAbandonedEvent {
   constructor(
@@ -7,5 +8,6 @@ export class PlayerAbandonedEvent {
     public readonly matchId: number,
     public readonly abandonIndex: number,
     public readonly mode: MatchmakingMode,
+    public readonly gameState: Dota_GameRulesState,
   ) {}
 }
