14796922dadbe69b68c44148018cde0b7b5b4bbb
Yuri Stepin <enchantinggg4@gmail.com>
2025-08-23 13:49:59 +0300

    player finished match event

---
+++ events/gs/player-finished-match.event.ts
@@ -0,0 +1,11 @@
+import { MatchmakingMode } from 'shared-types/matchmaking-mode';
+
+export class PlayerFinishedMatchEvent {
+  constructor(
+    public readonly matchId: number,
+    public readonly steamId: string,
+    public readonly lobbyType: MatchmakingMode,
+    public readonly isFirstGame: boolean
+  ) {
+  }
+}
