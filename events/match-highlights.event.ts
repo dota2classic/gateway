f8229c8932396308b479c120906ac31922d5bbc8
Yuri Stepin <enchantinggg4@gmail.com>
2025-09-24 00:21:33 +0300

    Highlights stuff

---
+++ events/match-highlights.event.ts
@@ -0,0 +1,25 @@
+import { MatchmakingMode } from "../shared-types/matchmaking-mode";
+
+export enum HighlightType {
+  QUICK_MULTIKILL = "QUICK_MULTIKILL",
+  MULTIKILL = "MULTIKILL",
+  LOW_HP_KILL = "LOW_HP_KILL",
+  COMBO_SPELL = "COMBO_SPELL",
+}
+
+export class HighlightDTO {
+  constructor(
+    public gameTime: number,
+    public hero: string,
+    public type: HighlightType,
+    public comment: string,
+  ) {}
+}
+
+export class MatchHighlightsEvent {
+  constructor(
+    public matchId: number,
+    public lobbyType: MatchmakingMode,
+    public highlights: HighlightDTO[],
+  ) {}
+}
