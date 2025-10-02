b67466c0abb5cd630e35c003c9f4d3cee93ecea3
Yuri Stepin <enchantinggg4@gmail.com>
2025-10-02 22:24:07 +0300

    Extend event

--- events/gs/player-finished-match.event.ts
+++ events/gs/player-finished-match.event.ts
@@ -5,7 +5,9 @@ export class PlayerFinishedMatchEvent {
     public readonly matchId: number,
     public readonly steamId: string,
     public readonly lobbyType: MatchmakingMode,
-    public readonly isFirstGame: boolean
+    public readonly isFirstGame: boolean,
+    public readonly unrankedGamesCount: number,
+    public readonly anyGamesCount: number,
   ) {
   }
 }
