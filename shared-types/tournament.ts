940f9b94a2e18236d70b52916c854e553bad6069
Yuri Stepin <enchantinggg4@gmail.com>
2026-01-28 00:23:56 +0300

    Wow, thats some basic tournament stuff

--- shared-types/tournament.ts
+++ shared-types/tournament.ts
@@ -10,3 +10,19 @@ export enum TournamentStatus {
   IN_PROGRESS = 'IN_PROGRESS',
   FINISHED = 'FINISHED',
 }
+
+
+export enum MatchStatus {
+  /** The two matches leading to this one are not completed yet. */
+  Locked = 0,
+  /** One participant is ready and waiting for the other one. */
+  Waiting = 1,
+  /** Both participants are ready to start. */
+  Ready = 2,
+  /** The match is running. */
+  Running = 3,
+  /** The match is completed. */
+  Completed = 4,
+  /** At least one participant completed his following match. */
+  Archived = 5
+}
