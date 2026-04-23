7bfb8bf35c1c8b4d7606f07b41980bb048e09cac
Yuri Stepin <enchantinggg4@gmail.com>
2026-04-23 19:30:40 +0300

    extend event

--- events/mm/player-declined-game.event.ts
+++ events/mm/player-declined-game.event.ts
@@ -1,8 +1,14 @@
 import { MatchmakingMode } from "../../shared-types/matchmaking-mode";
 
+export enum DeclineReason {
+  DECLINED = "DECLINED",
+  TIMEOUT = "TIMEOUT",
+}
+
 export class PlayerDeclinedGameEvent {
   constructor(
     public readonly steamId: string,
     public readonly mode: MatchmakingMode,
+    public readonly reason: DeclineReason = DeclineReason.DECLINED,
   ) {}
 }
